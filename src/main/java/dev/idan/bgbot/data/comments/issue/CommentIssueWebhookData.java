4cb4ea445eb722d722a539725b298c3834d676d4
idan <idankob@gmail.com>
2023-09-14 22:52:19 +0300

    Fixed commentIssue event

--- src/main/java/dev/idan/bgbot/data/comments/issue/CommentIssueWebhookData.java
+++ src/main/java/dev/idan/bgbot/data/comments/issue/CommentIssueWebhookData.java
@@ -2,10 +2,7 @@ package dev.idan.bgbot.data.comments.issue;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
-import dev.idan.bgbot.data.WebhookProjectData;
-import dev.idan.bgbot.data.WebhookRepositoryData;
 import dev.idan.bgbot.data.combined.data.IssueCommentMergeData;
-import dev.idan.bgbot.data.combined.data.IssueCommentMergePipelineUserData;
 import dev.idan.bgbot.data.comments.object.attributes.CommentIssueObjectAttributesData;
 import dev.idan.bgbot.entities.Token;
 import net.dv8tion.jda.api.EmbedBuilder;
@@ -14,6 +11,7 @@ import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 @JsonTypeName("note")
 public class CommentIssueWebhookData extends IssueCommentMergeData {
 
+    @JsonProperty("issue")
     private Issue issue;
 
     @JsonProperty("object_attributes")
@@ -24,11 +22,12 @@ public class CommentIssueWebhookData extends IssueCommentMergeData {
         String userName = getUser().getName();
         String noteableType = objectAttributes.getNoteableType();
 
-        if (objectAttributes.getNoteableType().equals("Issue"))
+        if (objectAttributes.getNoteableType().equals("Issue")) {
             builder.setTitle(
                     userName + " Commented on " + noteableType
                             + ": " + issue.getTitle() +
                             " (#" + issue.getId() + ")", objectAttributes.getUrl());
+        }
 
         builder.setTitle(String.format("%s Commented on %s", userName, noteableType), objectAttributes.getUrl());
         builder.setDescription(objectAttributes.getNote());
--- src/main/java/dev/idan/bgbot/data/merge/MergeWebhookData.java
+++ src/main/java/dev/idan/bgbot/data/merge/MergeWebhookData.java
@@ -2,16 +2,12 @@ package dev.idan.bgbot.data.merge;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
-import dev.idan.bgbot.data.WebhookData;
-import dev.idan.bgbot.data.WebhookProjectData;
-import dev.idan.bgbot.data.WebhookRepositoryData;
 import dev.idan.bgbot.data.combined.data.IssueCommentMergeData;
-import dev.idan.bgbot.data.combined.data.IssueCommentMergePipelineUserData;
 import dev.idan.bgbot.entities.Token;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 
-@JsonTypeName("merge")
+@JsonTypeName("merge_request")
 public class MergeWebhookData extends IssueCommentMergeData {
 
     @JsonProperty("object_attributes")
@@ -19,8 +15,7 @@ public class MergeWebhookData extends IssueCommentMergeData {
 
     @Override
     public void apply(EmbedBuilder builder, String instanceURL, Token token, TextChannel channel) {
-        builder.setTitle(String.format("%s %s merge request from %s to %s",
-                getUser(),
+        builder.setTitle(String.format("%s merge request from %s to %s",
                 objectAttributes.getAction(),
                 objectAttributes.getSourceBranch(),
                 objectAttributes.getTargetBranch()
