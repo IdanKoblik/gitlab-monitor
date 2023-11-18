88d1567f4fffd92c072954743534321b567fe432
idan <idankob@gmail.com>
2023-11-18 21:11:23 +0200

    writerside base

--- src/main/java/dev/idan/bgbot/data/deployment/DeploymentWebhookData.java
+++
@@ -1,7 +0,0 @@
-package dev.idan.bgbot.data.deployment;
-
-import com.fasterxml.jackson.annotation.JsonTypeName;
-import dev.idan.bgbot.data.combined.data.BuildDeploymentData;
-
-@JsonTypeName("deployment")
-public class DeploymentWebhookData extends BuildDeploymentData {}
--- src/main/java/dev/idan/bgbot/utils/CommitHelper.java
+++
@@ -1,6 +0,0 @@
-package dev.idan.bgbot.utils;
-
-public class CommitHelper {
-
-    public static final String EMPTY_COMMIT_SHA = "0000000000000000000000000000000000000000";
-}
