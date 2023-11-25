64502e0e90fb72858948aac81f015f1fc2cc0169
Idan <idankob@gmail.com>
2023-11-25 15:20:04 +0200

    Update IssueService.java

--- src/main/java/dev/idan/bgbot/services/IssueService.java
+++ src/main/java/dev/idan/bgbot/services/IssueService.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -44,10 +43,4 @@ public class IssueService {
         }
 
     }
-
-    public ResponseEntity<?> createIssueResponse(String projectId, String title, String description) {
-        createIssue(projectId, title, description);
-        return ResponseEntity.ok("Successfully created an issue. ✅");
-    }
-
 }
